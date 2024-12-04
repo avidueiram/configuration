1805d2e8c0a2d49ad6ef936add944a1e5e21c91d
Alfredo Vidueira <vidueira1992@gmail.com>
2024-12-04 09:08:11 -0300

    cursor-config bash script added

---
+++ cursor-config.sh
@@ -0,0 +1,8 @@
+cursor --install-extension dbaeumer.vscursor-eslint
+cursor --install-extension eenaree.webstorm-new-dark
+cursor --install-extension golang.go
+cursor --install-extension k--kato.intellij-idea-keybindings
+cursor --install-extension mechatroner.rainbow-csv
+cursor --install-extension streetsidesoftware.cursor-spell-checker
+cursor --install-extension toba.vsfire
+cursor --install-extension tomoki1207.pdf
