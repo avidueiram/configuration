0c867d54e6ecf305086e9b31c359cdeb09c71f0d
Alfredo Vidueira <vidueira1992@gmail.com>
2025-01-08 15:53:23 -0300

    feat: backup-cursor-config.sh script added

---
+++ backup-cursor-config.sh
@@ -0,0 +1,18 @@
+echo "#!/bin/bash" > cursor-config.sh
+echo "\n# Copy settings to cursor" >> cursor-config.sh
+
+cp -r $HOME/Library/Application\ Support/Cursor/User/settings.json cursor-settings.json
+cp -r $HOME/Library/Application\ Support/Cursor/User/keybindings.json cursor-keybindings.json
+cp -r $HOME/Library/Application\ Support/Cursor/User/snippets cursor-snippets
+
+echo "cp -r cursor-settings.json \$HOME/Library/Application\ Support/Code/User/settings.json" >> cursor-config.sh
+echo "cp -r cursor-keybindings.json \$HOME/Library/Application\ Support/Code/User/keybindings.json" >> cursor-config.sh
+echo "cp -r cursor-snippets \$HOME/Library/Application\ Support/Code/User/snippets" >> cursor-config.sh
+
+echo "\n# Install extensions to cursor" >> cursor-config.sh
+
+cursor --list-extensions | while read extension; do
+    echo "cursor --install-extension $extension" >> cursor-config.sh
+done
+
+echo "\necho \"Cursor config installed\"" >> cursor-config.sh
--- cursor-config.sh
+++ cursor-config.sh
@@ -1,8 +1,22 @@
-cursor --install-extension dbaeumer.vscursor-eslint
+#!/bin/bash
+
+# Copy settings to cursor
+cp -r cursor-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+cp -r cursor-keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+cp -r cursor-snippets $HOME/Library/Application\ Support/Code/User/snippets
+
+# Install extensions to cursor
+cursor --install-extension angular.ng-template
+cursor --install-extension dbaeumer.vscode-eslint
 cursor --install-extension eenaree.webstorm-new-dark
 cursor --install-extension golang.go
 cursor --install-extension k--kato.intellij-idea-keybindings
 cursor --install-extension mechatroner.rainbow-csv
-cursor --install-extension streetsidesoftware.cursor-spell-checker
+cursor --install-extension ms-python.debugpy
+cursor --install-extension ms-python.python
+cursor --install-extension ms-python.vscode-pylance
+cursor --install-extension streetsidesoftware.code-spell-checker
 cursor --install-extension toba.vsfire
 cursor --install-extension tomoki1207.pdf
+
+echo "Cursor config installed"
